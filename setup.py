3a7d715ad0267a7a1e4200c90d7c6506470903c4
danielgtaylor <dan@programmer-art.org>
2010-05-29 20:40:29 -0400

    Update trove URL to point to new website, updated already on PyPi

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ Note: WebM support requires at least GStreamer's gst-plugins-bad-0.10.19.
 """,
     author = "Daniel G. Taylor",
     author_email = "dan@programmer-art.org",
-    url = "http://programmer-art.org/projects/arista-transcoder",
+    url = "http://www.transcoder.org/",
     download_url = "http://programmer-art.org/media/releases/arista-transcoder/arista-0.9.4.tar.gz",
     packages = [
         "arista",
