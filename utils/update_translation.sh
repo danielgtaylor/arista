987366087aea0e1d317dc79d8db5ee6ae6367e94
danielgtaylor <dan@programmer-art.org>
2010-06-01 17:03:19 -0400

    Update translation template generation script

--- utils/update_translation.sh
+++ utils/update_translation.sh
@@ -2,6 +2,6 @@
 
 # Extract all translatable messages to update the Arista PO template
 
-xgettext -L glade -o locale/templates/arista.pot ui/*.ui
-xgettext -L python -o - arista-gtk arista-transcode arista/*.py | tail -n +18 >>locale/templates/arista.pot
+xgettext -L glade -o - ui/*.ui | tail -n +6 >locale/templates/arista.pot
+xgettext -L python -o - arista-gtk arista-transcode arista/*.py arista/inputs/*.py | tail -n +18 >>locale/templates/arista.pot
 
