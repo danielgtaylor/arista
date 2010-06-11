32d2ea60d2d4dd63fd6bd769c3c2106718aae331
danielgtaylor <dan@programmer-art.org>
2010-06-10 23:28:13 -0400

    Update script to update translation template file to also include new nautilus script

--- utils/update_translation.sh
+++ utils/update_translation.sh
@@ -3,5 +3,5 @@
 # Extract all translatable messages to update the Arista PO template
 
 xgettext -L glade -o - ui/*.ui | tail -n +6 >locale/templates/arista.pot
-xgettext -L python -o - arista-gtk arista-transcode arista/*.py arista/inputs/*.py | tail -n +18 >>locale/templates/arista.pot
+xgettext -L python -o - arista-gtk arista-transcode arista-nautilus.py arista/*.py arista/inputs/*.py | tail -n +18 >>locale/templates/arista.pot
 
