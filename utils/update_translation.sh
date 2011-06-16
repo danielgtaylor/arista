99486272581fa451e4b888cfd68a96784544f949
Daniel G. Taylor <dan@programmer-art.org>
2011-06-15 20:25:04 -0400

    Fix small bug in translation update script

--- utils/update_translation.sh
+++ utils/update_translation.sh
@@ -2,6 +2,6 @@
 
 # Extract all translatable messages to update the Arista PO template
 
-xgettext -L glade -o locale/templates/arista.pot ui/*.ui | tail -n +6 >
+xgettext -L glade -o locale/templates/arista.pot ui/*.ui
 xgettext -L python -o locale/templates/arista.pot -j arista-gtk arista-transcode arista-nautilus.py arista/*.py arista/inputs/*.py
 
