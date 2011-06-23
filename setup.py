bcbb88e8582cbdc2c395dee3e13d5e1ad00db5f3
malcolmlewis <malcolmlewis@opensuse.org>
2011-06-23 07:30:47 -0500

    Add missing man pages

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ if not hasattr(DistributionMetadata, "zip_safe"):
 
 data_files = [
     (os.path.join("share", "applications"), ["arista.desktop"]),
+    (os.path.join("share", "man/man1"), [
+        "man/arista-gtk.1", "man/arista-transcode.1"]),
     (os.path.join("share", "doc", "arista"), [
         "README.md", "LICENSE", "AUTHORS"
     ]),
